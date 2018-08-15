9acd4f5a2e7b4895a096e9057e45e20d0ce39dcb
Manikandan Sundararajan <msundararajan@derivetech.com>
2018-08-15 13:21:23 -0400

    Implement arithmetic ops for Fraction class

--- fraction.py
+++ fraction.py
@@ -1,17 +1,17 @@
 #Python is object oriented. We can use classes to implement an abstract
 #data type
 
+#Function to calculate the greatest common denominator between 2 numbers
 def gcd(m, n):
-    while m%n != 0:
+    while (m % n) != 0:
         oldm = m
         oldn = n
-        
         m = oldn
         n = oldm%oldn
     return n
 
+#Class to implement Fraction functionalilty
 class Fraction:
-
     #This is the constructor method for the class
     #The self parameter is used as a reference back to the object itself
     def __init__(self, top, bottom):
@@ -19,9 +19,10 @@ class Fraction:
         self.num = top
         self.den = bottom
 
+    #Class method to provide string representation
     def show(self):
         print("%i/%i" %(self.num, self.den))
-    
+        
     #Function to convert the object to a string representation
     #Will allow the object to be printed out
     def __str__(self):
@@ -29,9 +30,9 @@ class Fraction:
 
     #This function allows the + operator to understand Fraction operands
     #It overrides the addition method
-    def __add__(self, otherfraction):
-        newnum = self.num*otherfraction.den + self.den*otherfraction.num
-        newden = self.den*otherfraction.den
+    def __add__(self, other):
+        newnum = self.num*other.den + self.den*other.num
+        newden = self.den*other.den
         common = gcd(newnum, newden)
         return Fraction(newnum//common, newden//common)
 
@@ -42,15 +43,49 @@ class Fraction:
         secondnum = other.num * self.den
 
         return firstnum == secondnum
-    
 
+    #Function to overload the - operator
+    def __sub__(self, other):
+        newnum = self.num*other.den - self.den*other.num
+        newden = self.den*other.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum//common, newden//common)
 
-myf = Fraction(3,5)
-print(myf)
+    #Function to overload the * operator
+    def __mul__(self, other):
+        newnum = self.num * other.num
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum//common, newden//common)
 
-print("I ate", myf, "of the pizza")
+    #Function to overload the / operator
+    def __truediv__(self, other):
+        newnum = self.num * other.den
+        newden = self.den * other.num
+        common = gcd(newnum, newden)
+        return Fraction(newnum//common, newden//common)
 
-f1 = Fraction(1,4)
-f2 = Fraction(1,2)
+    #Function to overload the < operator
+    def __lt__(self, other):
+        return (self.num / self.den) < (other.num / other.den)
 
-print(f1+f2)
+    #Function to overload the > operator
+    def __gt__(self, other):
+        return (self.num / self.den) > (other.num / other.den)
+
+F4 = Fraction(3, 5)
+print(F4)
+
+print("I ate", F4, "of the pizza")
+
+F1 = Fraction(1, 4)
+F2 = Fraction(1, 2)
+F3 = Fraction(1, 2)
+
+print(F1 + F2)
+print(F3 == F2)
+print(F2 - F1)
+print(F4 * F4)
+print(F2 / F1)
+print(F3 < F2)
+print(F4 > F2)
